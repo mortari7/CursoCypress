8b95719db3adc4f429376e34103caf6ea4f430b1
Alessandro Mortari <mortari7@hotmail.com>
2020-11-26 16:35:37 -0300

    Alteração nos Testes

--- cypress/integration/tickets.spec.js
+++ cypress/integration/tickets.spec.js
@@ -1,8 +1,16 @@
-const { describe } = require("mocha");
-
 describe("Tickets", () =>{
-    beforeEach(() => cy.visit("https://ticket-box.s3.eu-central-1.amazonaws.com/index.html"));
+    beforeEach(() => cy.visit("https://ticket-box.s3.eu-central-1.amazonaws.com/index.html"))
     it("Has 'Ticket-Box' header's heading", ()=>{
-
     });
+
+    it("Fills all the text input fields", () =>{
+        const firstName = "Alessandro";
+        const lastName = "Mortari";
+        cy.get("#first-name").type(firstName);
+        cy.get("#last-name").type(lastName);
+        cy.get("#email").type("mortari7@hotmail.com");
+        cy.get("#requests").type("teste");
+        cy.get("#signature").type(`${firstName} ${lastName}`);
+    });
+    
 });
