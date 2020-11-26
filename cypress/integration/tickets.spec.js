d58dc449c388e396217dc810873c11409f9db155
Alessandro Mortari <mortari7@hotmail.com>
2020-11-26 18:42:25 -0300

    Atualização do Curso Cypress

--- cypress/integration/tickets.spec.js
+++ cypress/integration/tickets.spec.js
@@ -2,7 +2,6 @@ describe("Tickets", () =>{
     beforeEach(() => cy.visit("https://ticket-box.s3.eu-central-1.amazonaws.com/index.html"))
     it("Has 'Ticket-Box' header's heading", ()=>{
     });
-
     it("Fills all the text input fields", () =>{
         const firstName = "Alessandro";
         const lastName = "Mortari";
@@ -12,5 +11,12 @@ describe("Tickets", () =>{
         cy.get("#requests").type("teste");
         cy.get("#signature").type(`${firstName} ${lastName}`);
     });
-    
+
+    it("Select two tickets", () =>{
+        cy.get("#ticket-quantity").select("2");
+    });
+
+    it("Select the vip ticket in radio button", () =>{
+        cy.get("#vip").check();
+    });    
 });
